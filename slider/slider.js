da586a680e6f46ff3d16a723de58c0ebda10e15b
Angéline <anje.jiro@gmail.com>
2023-01-20 16:27:56 +0100

    Continuation slider

--- slider/slider.js
+++ slider/slider.js
@@ -1,8 +1,14 @@
 let rightArrow = document.getElementById('right-arrow');
+let leftArrow = document.getElementById('left-arrow');
 let listSlide = document.getElementById('list-slide');
 let mLeft = 0;
 
 rightArrow.addEventListener('click',function(){
     mLeft -= 500;
+    listSlide.style.marginLeft = mLeft + 'px';
+});
+
+leftArrow.addEventListener('click',function(){
+    mLeft += 500;
     listSlide.style.marginLeft = mLeft + 'px';
 });
