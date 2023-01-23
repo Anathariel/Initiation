2c84b1eabf6c40320e8c6502cc76b7bef323a655
Angéline <anje.jiro@gmail.com>
2023-01-23 16:55:45 +0100

    Slidder stop aux dernières slides

--- slider/slider.js
+++ slider/slider.js
@@ -1,14 +1,19 @@
 let rightArrow = document.getElementById('right-arrow');
 let leftArrow = document.getElementById('left-arrow');
 let listSlide = document.getElementById('list-slide');
+let slides = document.getElementsByClassName('slide')
 let mLeft = 0;
 
-rightArrow.addEventListener('click',function(){
-    mLeft -= 500;
-    listSlide.style.marginLeft = mLeft + 'px';
+rightArrow.addEventListener('click', function () {
+    if (mLeft - 500 >= -2000) {
+        mLeft -= 500;
+        listSlide.style.marginLeft = mLeft + 'px';
+    }
 });
 
-leftArrow.addEventListener('click',function(){
-    mLeft += 500;
-    listSlide.style.marginLeft = mLeft + 'px';
+leftArrow.addEventListener('click', function () {
+    if (mLeft + 500 <= 0) {
+        mLeft += 500;
+        listSlide.style.marginLeft = mLeft + 'px';
+    }
 });
